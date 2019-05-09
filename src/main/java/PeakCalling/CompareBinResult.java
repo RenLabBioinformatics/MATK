2eec4db19e88da5a1e7941393f4b8260a1a6041b
Ben <1063456489@qq.com>
2019-05-09 15:10:08 +0800

    2019.5.9 Fix some bug

--- src/main/java/PeakCalling/CompareBinResult.java
+++ src/main/java/PeakCalling/CompareBinResult.java
@@ -9,7 +9,7 @@ public class CompareBinResult implements Comparator<BinResult> {
     public int compare(BinResult o1, BinResult o2) {
         if(o1.getIndex() < o2.getIndex()) {
             return -1;
-        } else if ( o2.getIndex() > o2.getIndex()) {
+        } else if ( o1.getIndex() > o2.getIndex()) {
             return 1;
         } else {
             return 0;
